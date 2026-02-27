9658caff0cdf952cd3f5e762d7839bd0e1341582
avierkin03 <avierkin03@gmail.com>
2026-02-27 19:49:24 +0200

    added 2 views

--- task/views.py
+++ task/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Task
 
-# Create your views here.
+# Представлення списику існуючих завдань
+class TaskListView(ListView):
+    model = Task
+    context_object_name = "tasks"
+    template_name = "task/task_list.html"
+
+
+# Представлення деталей конкретного завдання
+class TaskDetailView(DetailView):
+    model = Task 
+    context_object_name = "task"
+    template_name = "task/task_detail.html"
